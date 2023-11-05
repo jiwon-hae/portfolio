397b84ddd52dfa29d0137676b8dbe5f2c70db417
Jiwon Hae <hjiwon1994@gmail.com>
2023-11-05 20:11:24 +0900

    chore : Fix codes for static analysis

--- lib/presentation/feature/home/home_screen.dart
+++ lib/presentation/feature/home/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:portfolio/presentation/feature/home/home_state.dart';

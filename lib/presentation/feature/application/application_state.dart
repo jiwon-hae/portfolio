caebb849efe9f0921fde3eb11ab92d371b5d9c07
Jiwon Hae <hjiwon1994@gmail.com>
2023-11-05 21:46:05 +0900

    feat : Update application state

--- lib/presentation/feature/application/application_state.dart
+++ lib/presentation/feature/application/application_state.dart
@@ -1,7 +1,12 @@
-class ApplicationState {
+import 'package:equatable/equatable.dart';
+
+class ApplicationState extends Equatable {
   const ApplicationState();
 
   factory ApplicationState.getDefault(){
     return const ApplicationState();
   }
+
+  @override
+  List<Object?> get props => <Object?>[];
 }
